b8aab4eeb202abaf80d1af02aa393eb3a200e2ae
Scott Schmitz <scottaschmitz@gmail.com>
2018-08-08 08:57:55 -0400

    WIP - Starting to get dialogue functionality

---
+++ Game/Assets/Scripts/GUI/DialogueHolder.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+    public string dialogue;
+
+    private DialogueManager dialogueManager;
+
+	// Use this for initialization
+	void Start () {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Game/Assets/Scripts/GUI/DialogueManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dialogueBox;
+    public Text dialogueText;
+
+    public bool dialogueActive;
+	
+	void Update () {
+        if (dialogueActive && Input.GetKeyDown(KeyCode.Space)) {
+            dialogueBox.SetActive(false);
+            dialogueActive = false;
+        }
+	}
+
+    public void showBox(string dialogue) {
+        dialogueActive = true;
+        dialogueBox.SetActive(true);
+        dialogueText.text = dialogue
+    }
+}
